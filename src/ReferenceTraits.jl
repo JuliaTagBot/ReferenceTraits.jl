d509a2d16d86f317356a477d33849287db4caa08
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-04-05 18:12:02 +0100

    Files generated by PkgTemplates

---
+++ src/ReferenceTraits.jl
@@ -0,0 +1,5 @@
+module ReferenceTraits
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ReferenceTraits
+using Test
+
+@testset "ReferenceTraits.jl" begin
+    # Write your own tests here.
+end
